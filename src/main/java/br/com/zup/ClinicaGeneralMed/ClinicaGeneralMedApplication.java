77a622509806091c03311f5a1cbe78e26fa21c4a
alefe.nunes <alefe.nunes@zup.com.br>
2021-08-04 13:59:25 -0300

    Iniciando projeto

---
+++ src/main/java/br/com/zup/ClinicaGeneralMed/ClinicaGeneralMedApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.ClinicaGeneralMed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaGeneralMedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaGeneralMedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/ClinicaGeneralMed/ClinicaGeneralMedApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.ClinicaGeneralMed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaGeneralMedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
