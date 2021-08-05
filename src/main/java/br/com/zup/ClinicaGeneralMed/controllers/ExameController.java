6a77c2378edf0fb2f355e7979d10affd593501e0
alefe.nunes <alefe.nunes@zup.com.br>
2021-08-05 17:42:00 -0300

    Implantanndo Exame Controller

--- src/main/java/br/com/zup/ClinicaGeneralMed/controllers/ExameController.java
+++ src/main/java/br/com/zup/ClinicaGeneralMed/controllers/ExameController.java
@@ -1,8 +1,28 @@
 package br.com.zup.ClinicaGeneralMed.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import br.com.zup.ClinicaGeneralMed.dtos.ExameDTO;
+import br.com.zup.ClinicaGeneralMed.services.ExameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-
+@RequestMapping("/exame")
 public class ExameController {
+
+    @Autowired
+    private ExameService exameService;
+
+    @PostMapping
+    public ExameDTO cadastrarExame(@RequestBody ExameDTO exameDTO) {
+        return exameService.cadastrarExame(exameDTO);
+    }
+
+    @GetMapping
+    public List<ExameDTO> exibirExames() {
+        return exameService.exibirExames();
+
+    }
+
 }
